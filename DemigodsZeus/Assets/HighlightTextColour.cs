d6c68a7c303e4d77afbcb2e1edd83fb538dcc599
KaylaG1510 <mikayla.p8920@gmail.com>
2020-09-06 16:49:25 +1200

    Selected Menu Highlight
    Triangle sprite follows selected menu option

--- DemigodsZeus/Assets/HighlightTextColour.cs
+++ DemigodsZeus/Assets/HighlightTextColour.cs
@@ -7,16 +7,32 @@ using UnityEngine.UI;
 public class HighlightTextColour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Text theText;
-    //?Burnt orange
+
+    //?Burnt Orange
     private Color32 highlightColour = new Color32(207, 78, 8, 255);
 
+    public GameObject t;
+    public float posX;
+    public float posY;
+
+    void Start()
+    {
+        t.GetComponent<Renderer>().enabled = false;
+    }
+
+    //When mouse hovers over button
     public void OnPointerEnter(PointerEventData eventData)
     {
         theText.color = highlightColour;
+
+        t.transform.position = new Vector3(posX, posY, 0);
+        t.GetComponent<Renderer>().enabled = true;
     }
 
+    //When mouse stops hovering over button
     public void OnPointerExit(PointerEventData eventData)
     {
         theText.color = Color.white;
+        t.GetComponent<Renderer>().enabled = false;
     }
 }
