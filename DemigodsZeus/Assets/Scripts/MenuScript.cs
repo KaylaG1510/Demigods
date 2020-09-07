d8faadbb97e91a57ea14b46cd734c770ba1fc7a9
KaylaG1510 <mikayla.p8920@gmail.com>
2020-09-07 14:20:15 +1200

    Setup Unit Testing and Adding Instruction Menu
    - Basis for unit testing added.
    - start of Instructions menu added so can add transitions between main menus

---
+++ DemigodsZeus/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    //When New Game button is pressed
+    public void OnNewGame()
+    {
+        
+    }
+
+    //When Instructions button is pressed
+    public void OnInstructions()
+    {
+        Debug.Log("You have entered Instructions Menu");
+    }
+
+    //When Character Selection button is pressed
+    public void OnCharacterSelection()
+    {
+
+    }
+
+    //When Options button is pressed
+    public void OnOptions()
+    {
+
+    }
+
+    //When Exit button is pressed
+    public void OnExit()
+    {
+
+    }
+}
---
+++ DemigodsZeus/Assets/Tests (PM)/MainMenuTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class MainMenuTest
+{
+    private MenuScript menu = new MenuScript();
+
+    [UnityTest]
+    public IEnumerator MainMenuStarts()
+    {
+        GameObject menuGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/MainMenu"));
+
+        menu = menuGameObject.GetComponent<MenuScript>();
+
+        yield return null;
+        //Tests Main Menu is active GameObject
+        Assert.IsTrue(menuGameObject.activeInHierarchy);
+    }
+}
+
