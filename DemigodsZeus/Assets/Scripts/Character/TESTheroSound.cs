109e9380c7d785fd216c2924d3d0fd406d5b468e
KaylaG1510 <mikayla.p8920@gmail.com>
2020-09-20 12:32:47 +1200

    Almost done pause menu
    just gotta pause background activity

--- DemigodsZeus/Assets/Scripts/Character/TESTheroSound.cs
+++ DemigodsZeus/Assets/Scripts/Character/TESTheroSound.cs
@@ -1,32 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroKnight : MonoBehaviour {
+public class TESTheroSound : MonoBehaviour
+{
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
-    [SerializeField] float      m_rollForce = 6.0f;
-    [SerializeField] bool       m_noBlood = false;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
+    [SerializeField] float m_rollForce = 6.0f;
+    [SerializeField] bool m_noBlood = false;
     [SerializeField] GameObject m_slideDust;
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_HeroKnight   m_groundSensor;
-    private Sensor_HeroKnight   m_wallSensorR1;
-    private Sensor_HeroKnight   m_wallSensorR2;
-    private Sensor_HeroKnight   m_wallSensorL1;
-    private Sensor_HeroKnight   m_wallSensorL2;
-    private bool                m_grounded = false;
-    private bool                m_rolling = false;
-    private int                 m_facingDirection = 1;
-    private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
-    private float               m_delayToIdle = 0.0f;
-
-    bool readyToClear;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_HeroKnight m_groundSensor;
+    private Sensor_HeroKnight m_wallSensorR1;
+    private Sensor_HeroKnight m_wallSensorR2;
+    private Sensor_HeroKnight m_wallSensorL1;
+    private Sensor_HeroKnight m_wallSensorL2;
+    private bool m_grounded = false;
+    private bool m_rolling = false;
+    private int m_facingDirection = 1;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private float m_delayToIdle = 0.0f;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
@@ -35,17 +34,17 @@ public class HeroKnight : MonoBehaviour {
         m_wallSensorR2 = transform.Find("WallSensor_R2").GetComponent<Sensor_HeroKnight>();
         m_wallSensorL1 = transform.Find("WallSensor_L1").GetComponent<Sensor_HeroKnight>();
         m_wallSensorL2 = transform.Find("WallSensor_L2").GetComponent<Sensor_HeroKnight>();
-       
+
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         //Game is over
         if (GameManager.IsGameOver())
-            {
-                return;
-            }
+        {
+            return;
+        }
 
         // Increase timer that controls attack combo
         m_timeSinceAttack += Time.deltaTime;
@@ -72,14 +71,14 @@ public class HeroKnight : MonoBehaviour {
         {
             GetComponent<SpriteRenderer>().flipX = false;
             m_facingDirection = 1;
-        }       
+        }
         else if (inputX < 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
             m_facingDirection = -1;
         }
 
-        // Move
+        //Move
         if (!m_rolling)
         {
             m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
@@ -93,18 +92,18 @@ public class HeroKnight : MonoBehaviour {
         m_animator.SetBool("WallSlide", (m_wallSensorR1.State() && m_wallSensorR2.State()) || (m_wallSensorL1.State() && m_wallSensorL2.State()));
 
         //Death
-        if (Input.GetKeyDown("s"))
-        {
-            m_animator.SetBool("noBlood", m_noBlood);
-            m_animator.SetTrigger("Death");
-        }
+        //if (Input.GetKeyDown("s"))
+        //{
+        //    m_animator.SetBool("noBlood", m_noBlood);
+        //    m_animator.SetTrigger("Death");
+        //}
 
         //Hurt
         //else if (Input.GetKeyDown("q"))
         //    m_animator.SetTrigger("Hurt");
 
-        //Attack
-        else if(Input.GetKeyDown("w") && m_timeSinceAttack > 0.25f)
+        //Attack *** used to be else if
+        if (Input.GetKeyDown("w") && m_timeSinceAttack > 0.25f)
         {
             m_currentAttack++;
 
@@ -123,12 +122,12 @@ public class HeroKnight : MonoBehaviour {
             m_timeSinceAttack = 0.0f;
         }
         // Block
-        else if(Input.GetKeyDown("e"))
+        else if (Input.GetKeyDown("e"))
         {
             m_animator.SetTrigger("Block");
             m_animator.SetBool("IdleBlock", true);
         }
-        else if(Input.GetKeyUp("e"))
+        else if (Input.GetKeyUp("e"))
         {
             m_animator.SetBool("IdleBlock", false);
         }
@@ -160,8 +159,8 @@ public class HeroKnight : MonoBehaviour {
         {
             // Prevents flickering transitions to idle
             m_delayToIdle -= Time.deltaTime;
-                if(m_delayToIdle < 0)
-                    m_animator.SetInteger("AnimState", 0);
+            if (m_delayToIdle < 0)
+                m_animator.SetInteger("AnimState", 0);
         }
     }
 
--- DemigodsZeus/Assets/Scripts/MenuScripts/PauseMenu.cs
+++ DemigodsZeus/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -14,21 +15,42 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
     }
 
-    //User clicks pause button
+    //User clicks pause button, pause game and display menu
     public void OnPause()
     {
         Debug.Log("Pause button pressed :D");
+        //hide pause button whilst paused
         pauseMenu.SetActive(true);
         thisButton.SetActive(false);
     }
 
-    //user clicks resume button
+    //user clicks resume button, return to game
     public void OnResume()
     {
         Debug.Log("Resume activated");
+        //disable pause menu and reactivate pause button
         pauseMenu.SetActive(false);
         thisButton.SetActive(true);
     }
 
+    //user clicks restart button, restart scene
+    public void OnRestart()
+    {
+        Debug.Log("Restart button clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
+    //user clicks return to main menu button, load MainMenu scene (0)
+    public void OnMainMenu()
+    {
+        Debug.Log("Return to main menu button clicked");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    //user clicks exit game button, closes game application
+    public void OnExit()
+    {
+        Debug.Log("Exit application button clicked");
+        Application.Quit();
+    }
 }
