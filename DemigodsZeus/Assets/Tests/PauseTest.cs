4550fd9936c6b291a0ee39500183c18ed75b782f
KaylaG1510 <mikayla.p8920@gmail.com>
2020-09-20 14:23:34 +1200

    gameplayFreezes test keeps disappearing
    push

---
+++ DemigodsZeus/Assets/Tests/PauseTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace TestPause
+{
+    public class PauseTest : UITest
+    {
+        [UnityTest]
+        public IEnumerator pauseMenuDisplays()
+        {
+            yield return LoadScene("LevelOne");
+
+            yield return new WaitForSecondsRealtime(2);
+
+            yield return Press("PauseButton");
+
+            yield return WaitFor(new ObjectAppeared("PauseMenu"));
+        }
+
+        [UnityTest]
+        public IEnumerator gameplayFreezes()
+        {
+            yield return LoadScene("LevelOne");
+
+            yield return Press("PauseButton");
+
+            yield return WaitFor(new ObjectAppeared("PauseMenu"));
+
+            //yield return WaitFor(new BoolCondition(() => Time.timeScale == 0));
+
+
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player").gameObject;
+            //this.animator.GetCurrentAnimatorStateInfo(0).IsName("YourAnimationName");
+            //playerObject = GameObject.Instantiate(Resources.Load("Prefabs/Hero")) as GameObject;
+
+            Animator currentAnim = playerObject.GetComponent<Animator>();
+
+            yield return WaitFor(new BoolCondition(() => currentAnim.updateMode == AnimatorUpdateMode.Normal));
+        }
+
+        [UnityTest]
+        public IEnumerator sceneRestarts()
+        {
+            yield return null;
+        }
+    }
+}
