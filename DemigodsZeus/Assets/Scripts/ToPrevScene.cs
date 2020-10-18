85c18a5d23ea1ef6afa1f2dd429952cbe6595088
BroItsBren <jzr5192@autuni.ac.nz>
2020-10-18 23:50:57 +1300

    Next/Prev Scene
    Character can go to level 1 - 2 and 3- and can go back i.e. level 2 back to level 1

---
+++ DemigodsZeus/Assets/Scripts/ToPrevScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToPrevScene : MonoBehaviour
+{
+    private int prevSceneToLoad;
+    void Start()
+    {
+        prevSceneToLoad = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(prevSceneToLoad);
+    }
+}
