aa705e1c6167931686e7522797d049b838d24992
BroItsBren <jzr5192@autuni.ac.nz>
2020-10-24 02:05:36 +1300

    intro cutscene
    pls

---
+++ DemigodsZeus/Assets/Scripts/DialogueLine.cs
@@ -0,0 +1,27 @@
+﻿using DialogueSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+        [SerializeField] private string input;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder));
+
+        }
+
+    }
+
+}
