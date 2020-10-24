607a78f7fbea5b29a1edfb7fc8174c0fe711dee5
BroItsBren <jzr5192@autuni.ac.nz>
2020-10-25 03:13:34 +1300

    Dialogue Refined
    Ignore reaper at the start will move him down the end of the level later. Just easier to test atm when hes closer to the hero

---
+++ DemigodsZeus/Assets/Scripts/Dialogue/DialogueLine.cs
@@ -0,0 +1,65 @@
+﻿using DialogueSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+        [Header ("Text Options")]
+        [SerializeField] private string input;
+
+        [Header("Time Parameters")]
+        [SerializeField] private float delay;
+        [SerializeField] private float delayBetweenLines;
+
+        [Header("Character Image")]
+        [SerializeField] private Sprite characterSprite;
+        [SerializeField] private Image imageHolder;
+
+        private IEnumerator lineAppear;
+
+        private void Awake()
+        {
+            imageHolder.sprite = characterSprite;
+            imageHolder.preserveAspect = true;
+        }
+
+        private void OnEnable()
+        {
+            ResetLine();
+            textHolder = GetComponent<Text>();
+            textHolder.text = "";
+            lineAppear = WriteText(input, textHolder, delay, delayBetweenLines);
+            StartCoroutine(lineAppear);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(textHolder.text != input)
+                {
+                    StopCoroutine(lineAppear);
+                    textHolder.text = input;
+                }
+                else
+                {
+                    finished = true;
+                }
+            }
+        }
+
+        private void ResetLine()
+        {
+            textHolder = GetComponent<Text>();
+            textHolder.text = "";
+            finished = false;
+        }
+
+    }
+
+}
