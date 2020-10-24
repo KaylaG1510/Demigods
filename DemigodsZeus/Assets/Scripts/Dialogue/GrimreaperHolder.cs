b0f2c70a4565b670569ff56a1ba3a6cc56b620fd
BroItsBren <jzr5192@autuni.ac.nz>
2020-10-25 04:04:42 +1300

    editing end of level one dialogue
    midway through

---
+++ DemigodsZeus/Assets/Scripts/Dialogue/GrimreaperHolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class GrimreaperHolder : MonoBehaviour
+    {
+        private IEnumerator dialogueSeq;
+        private void OnEnable()
+        {
+            dialogueSeq = DialogueSequence();
+            StartCoroutine(dialogueSeq);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.C))
+            {
+                Deactivate();
+                gameObject.SetActive(false);
+                StopCoroutine(dialogueSeq);
+            }
+        }
+
+        private IEnumerator DialogueSequence()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Deactivate();
+                transform.GetChild(i).gameObject.SetActive(true);
+                yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
+            }
+            gameObject.SetActive(false);
+        }
+
+        private void Deactivate()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
