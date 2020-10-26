8ce4c2437c4af4f9653dd7c2b7c938c1421bc333
BroItsBren <jzr5192@autuni.ac.nz>
2020-10-27 00:53:34 +1300

    push
    push

--- DemigodsZeus/Assets/Minotaur.cs
+++ DemigodsZeus/Assets/Minotaur.cs
@@ -7,7 +7,6 @@ public class Minotaur : MonoBehaviour
 {
     public float speed;
     public bool movingRight;
-    //public bool isFlipped;
     public bool Attacking;
     //public Transform groundDetection;
     //private Sensor_Bandit m_groundSensor;
@@ -18,12 +17,11 @@ public class Minotaur : MonoBehaviour
     public bool Stunned;
     public bool Dying;
 
-    public Rigidbody2D m_body2d;
+    private Rigidbody2D m_body2d;
     public Animator m_animator;
-    private Transform playerTransform;
 
     public float attackRange;
-    public double damage;
+    public int damage;
     private float lastAttackTime;
     public float attackDelay;
     public GameObject playerTarget;
@@ -35,18 +33,15 @@ public class Minotaur : MonoBehaviour
     private bool ChargeStarted;
     private float ChargeTimer;
 
-    string m_scene;
-
     // Start is called before the first frame update
     void Start()
     {
         //initialise all variables from raycast player, controller and
         //bandit ai code
-        //speed = -100;
+        speed = -1;
         movingRight = true;
         Attacking = false;
-        isFlipped = false;
-        //maxHealth = 500;
+        maxHealth = 500;
         playerTarget = GameObject.FindGameObjectWithTag("Player");
         attackDelay = 2.5f;
         damage = 30;
@@ -57,49 +52,22 @@ public class Minotaur : MonoBehaviour
         Stunned = false;
         currentHealth = maxHealth;
         healthBar.SetMaxHealth((int)maxHealth);
-        playerTransform = playerTarget.GetComponent<Transform>();
-
-        //level 2 slower speed and less health
-        //level 3 faster, less attack delay and more damage(double health)
-        m_scene = SceneManager.GetActiveScene().name;
-        if (m_scene.CompareTo("LevelTwo") == 0)
-        {
-            maxHealth = 500;
-        }
-        else if (m_scene.CompareTo("LevelThree") == 0)
-        {
-            maxHealth = 900;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         m_animator.SetBool("Alive", IsAlive);
-        //m_animator.SetBool("Walking", true);
+        m_animator.SetBool("Walking", true);
 
         //move minotaur
-        //transform.Translate(Vector2.left * speed * Time.deltaTime);
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
 
         if (Input.GetKeyDown("1"))
         {
             //test attacks
         }
 
-        //everytime player is in range and attack delay is up,
-        //call:
-        //attackMultiplier(chooseAttack());
-
-        float distToPlayer = Vector3.Distance(transform.position, playerTarget.transform.position);
-
-        //Vector2 targetPosition = new Vector2(playerTarget.transform.position.x, m_body2d.position.y);
-
-        //transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
-
-        ////Debug.Log(targetPosition.x + ", " + targetPosition.y);
-        //Vector2 newPos = Vector2.MoveTowards(m_body2d.position, targetPosition, speed * Time.deltaTime);
-        //m_body2d.MovePosition(newPos);
-        //transform.position = Vector3.MoveTowards(transform.position, target.position, step);
 
     }
 
@@ -112,20 +80,16 @@ public class Minotaur : MonoBehaviour
         {
             case 0:
                 attack = AttackType.Stomp;
-                Debug.Log("num: " + rand_num + "Attack Type: Stomp");
                 break;
             case 1:
                 attack = AttackType.Bash;
-                Debug.Log("num: " + rand_num + "Attack Type: Bash");
                 break;
             case 2:
                 attack = AttackType.Charge;
-                Debug.Log("num: " + rand_num + "Attack Type: Charge");
                 break;
             case 3:
             default:
                 attack = AttackType.Swing;
-                Debug.Log("num: " + rand_num + "Attack Type: Swing/Default");
                 break;
         }
 
@@ -146,19 +110,7 @@ public class Minotaur : MonoBehaviour
         //changes damage multiplier depending on attack
         //also deals additional multiplier if on level 3
 
-        switch(attackType)
-        {
-            case AttackType.Bash:
-                damage *= 1.5;
-                break;
-            case AttackType.Stomp:
-            case AttackType.Charge:
-                damage *= 1;
-                break;
-            case AttackType.Swing:
-                damage *= 1.25;
-                break;
-        }
+
 
         //stub
         return 2.0f;
@@ -288,26 +240,26 @@ public class Minotaur : MonoBehaviour
 
     }
 
-    //public void ChangeDirection()
-    //{
-    //    if(!movingRight)
-    //    {
-    //        transform.eulerAngles = new Vector3(0, 0, 0);
-    //        movingRight = true;
-    //    }
-    //    else
-    //    {
-    //        transform.eulerAngles = new Vector3(0, -180, 0);
-    //        movingRight = false;
-    //    }
-    //}
+    public void ChangeDirection()
+    {
+        if(!movingRight)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingRight = true;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            movingRight = false;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("EnemyBound"))
         {
             Debug.Log("Hit Bounds");
-            //ChangeDirection();
+            ChangeDirection();
         }
     }
 
