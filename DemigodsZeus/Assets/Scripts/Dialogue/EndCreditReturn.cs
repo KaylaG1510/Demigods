b978d5c5d6698ce2abfdf631ab70609d92d1c512
BroItsBren <jzr5192@autuni.ac.nz>
2020-10-27 15:16:15 +1300

    EndCredit To MainMenu
    Push

---
+++ DemigodsZeus/Assets/Scripts/Dialogue/EndCreditReturn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCreditReturn : MonoBehaviour
+{
+    public void toMainMenuScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public class PauseMenu : MonoBehaviour
+    {
+
+    }
+
+}
