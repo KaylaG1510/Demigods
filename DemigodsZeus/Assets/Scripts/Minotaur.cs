1a7f26722016bd77eecae84b13c496be5ba1586b
KaylaG1510 <mikayla.p8920@gmail.com>
2020-10-28 22:48:46 +1300

    stuff

--- DemigodsZeus/Assets/Scripts/Minotaur.cs
+++ DemigodsZeus/Assets/Scripts/Minotaur.cs
@@ -118,7 +118,7 @@ public class Minotaur : MonoBehaviour
 
                     ResetDamage();
                     lastAttackTime = Time.time;
-                    StopAttacks();
+                    //StopAttacks();
                 }
             }
         }
