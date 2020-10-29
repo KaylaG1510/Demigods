ef3e0f611fed61ea00516d3b2e37bce7f12ec52b
hgh4276 <hgh4276@autuni.ac.nz>
2020-10-29 15:34:11 +1300

    character selection
    character selection

---
+++ DemigodsZeus/Assets/Scripts/AnimatorOverrider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonoBehaviours
+{
+    public class AnimatorOverrider : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void SetAnimations(AnimatorOverrideController overiderController)
+        {
+            _animator.runtimeAnimatorController = overiderController;
+        }
+        
+    }
+}
---
+++ DemigodsZeus/Assets/SetAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonoBehaviours
+{
+    public class SetAnimation : MonoBehaviour
+    {
+        [SerializeField] private AnimatorOverrideController[] overrideControllers;
+        [SerializeField] private AnimatorOverrider overrider;
+
+        public void set(int value)
+        {
+            overrider.SetAnimations(overrideControllers[value]);
+        }
+    }
+}
